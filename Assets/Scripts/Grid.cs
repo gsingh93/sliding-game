69f25eee678b11b446281ed1fbedfb8d4c91ba84
Gulshan Singh <gulshan@umich.edu>
2014-02-15 22:42:10 -0500

    Added arrows to piece

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -16,34 +16,51 @@ public class Grid : MonoBehaviour {
 	private class Square {
 		public SquareType type;
 		public Player player;
+
+		public Square() {
+			type = SquareType.Empty;
+		}
 	}
 
 	private Square[,] grid;
 
 	void Awake() {
 		grid = new Square[Dim, Dim];
+		for (int i = 0; i < Dim; i++) {
+			for (int j = 0; j < Dim; j++) {
+				grid[i, j] = new Square();
+			}
+		}
+	}
+
+	public bool IsEmpty(int row, int col) {
+		BoundsCheck(row, col);
+
+		return grid[row, col].type == SquareType.Empty;
 	}
 
 	public Vector3 PosToCoord(int row, int col) {
-		if (!InBounds(row, col)) {
-			throw new System.ArgumentOutOfRangeException("Out of bounds: (" + row + ", " + col + ")");
-		}
+		BoundsCheck(row, col);
 
-		return new Vector3(-5.5f + col, -5.5f + row, -1);
+		return new Vector3(-5.5f + col, 5.5f - row, -1);
 	}
 
 	public Pair<int, int> CoordToPos(Vector3 coord) {
 		int row = (int) (coord.y + 5.5f);
 		int col = (int) (coord.x + 5.5f);
 
-		if (!InBounds(row, col)) {
-			throw new System.ArgumentOutOfRangeException("Out of bounds: (" + row + ", " + col + ")");
-		}
+		BoundsCheck(row, col);
 
 		return new Pair<int, int>(row, col);
 	}
 
+	private void BoundsCheck(int row, int col) {
+		if (!InBounds(row, col)) {
+			throw new System.ArgumentOutOfRangeException("Out of bounds: (" + row + ", " + col + ")");
+		}
+	}
+
 	private bool InBounds(int row, int col) {
-		return row >= Dim || col >= Dim || row < 0 || col < 0;
+		return row < Dim && col < Dim && row >= 0 && col >= 0;
 	}
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,6 +2,27 @@
 using System.Collections;
 
 public class Piece : MonoBehaviour {
+
+	public GameObject upArrowPrefab;
+	public GameObject downArrowPrefab;
+	public GameObject leftArrowPrefab;
+	public GameObject rightArrowPrefab;
+
+	private int[] dr = {-1, 1, 0, 0};
+	private int[] dc = {0, 0, 1, -1};
+
+	private int row, col;
+
+	private Grid g;
+
+	void Start() {
+		g = GameObject.Find("Board").GetComponent<Grid>();
+		Pair<int, int> p = g.CoordToPos(transform.position);
+		row = p.First;
+		col = p.Second;
+		CreateArrows();
+	}
+
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.A)) {
 			transform.rigidbody.velocity = Vector3.left * 10;
@@ -16,5 +37,31 @@ public class Piece : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision) {
 		//transform.rigidbody.velocity = Vector3.zero;
+	}
+
+	private void CreateArrows() {
+		for (int i = 0; i < dc.Length; i++) {
+			if (g.IsEmpty(row + dr[i], col + dc[i])) {
+				CreateArrow(i);
+			}
+		}
+	}
+
+	private void CreateArrow(int index) {
+		GameObject arrow;
+		if (dr[index] == 1) {         // Down
+			arrow = Instantiate(downArrowPrefab) as GameObject;
+		} else if (dr[index] == -1) { // Up
+			arrow = Instantiate(upArrowPrefab) as GameObject;
+		} else if (dc[index] == -1) { // Left
+			arrow = Instantiate(leftArrowPrefab) as GameObject;
+		} else if (dc[index] == 1) {  // Right
+			arrow = Instantiate(rightArrowPrefab) as GameObject;
+		} else {
+			throw new System.ArgumentException();
+		}
+
+		arrow.transform.parent = transform;
+		arrow.transform.position = transform.position + new Vector3(dc[index], -dr[index], 0);
 	}
 }
