d97fe7513d33360cc0ee194d5a111c0b04729d26
Gulshan Singh <gulshan@umich.edu>
2014-02-19 00:43:35 -0500

    Added menu scene with menu button animations

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+	public enum Button {
+		Play, Help
+	}
+	public Button button;
+
+	public void OnMouseEnter() {
+		GetComponent<Animator>().Play("Grow");
+		renderer.material.color = Color.red;
+	}
+
+	private void OnMouseExit() {
+		GetComponent<Animator>().Play("Shrink");
+		renderer.material.color = Color.white;
+	}
+
+	private void OnMouseDown() {
+		switch(button) {
+		case Button.Play:
+			break;
+		case Button.Help:
+			break;
+		}
+	}
+}
